b69e920d19b6cfe56bdca25fde6f932f5928d14a
Akemi Yoza <yoza.akemi@gmail.com>
2025-05-31 14:01:16 -0400

    create Heading component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
+import { TimerIcon } from "lucide-react";
 import { Heading } from "./components/Heading";
 
 import './styles/global.css';
 import './styles/theme.css';
 
 export function App() {
-  console.log('Oi');
 
   return (
     <>
-        <Heading/>
+        <Heading>Olá mundo!
+            <button>
+                <TimerIcon />
+            </button>
+        </Heading>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At nostrum iusto alias, autem neque perspiciatis rem quisquam ullam optio magni possimus dolore est soluta fugit doloribus, quasi ad excepturi repudiandae.</p>
     </>
   )
